336763d833614e69d768c48536ea2991419d8e5a
natalia <natalia@info.ufrn.com>
2020-03-03 00:15:45 -0300

    testando RT

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "film.hpp"
 #include "background.hpp"
 
-void RT()
+void RT_fixed()
 {
   int nx = 200;
   int ny = 100;
@@ -26,17 +26,41 @@ void RT()
   }
 }
 
+void RT(Film &film, Camera &cam, Background &bg)
+{
+  int nx = film.x_res;
+  int ny = film.y_res;
+  std::cout << "P3\n"
+            << nx << " " << ny << "\n"
+            << "255\n";
+  for (int y = 0; y < ny; y++)
+  {
+    for (int x = 0; x < nx; x++)
+    {
+      std::cout
+        // << int(255.99 * bg.color.r()) << " "
+        // << int(255.99 * bg.color.g()) << " "
+        // << int(255.99 * bg.color.b()) << "\n";
+        << int(bg.color.r()) << " "
+        << int(bg.color.g()) << " "
+        << int(bg.color.b()) << "\n";
+    }
+  }
+}
+
 int main(int argc, char const *argv[])
 {
   Camera camera;
   Film film;
   Background bg;
 
-  Parser parser("ex1.xml");
+  Parser parser("data/in/ex1.xml");
   parser.extractData(camera, film, bg);
 
-  film.print();
-  bg.print();
+  // film.print();
+  // bg.print();
+
+  RT(film, camera, bg);
   
   return 0;
 }
